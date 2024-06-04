a1739a4f68b2d03b7be0932a66afe1d246940954
Roberto Gomes <91014834+jr-roberto@users.noreply.github.com>
2024-06-04 15:55:52 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask import render_template
 from flask_cors import CORS
 
@@ -13,3 +13,7 @@ def index():
 @app.route("/cad_produto")
 def cad_produto():
     return render_template("cad_produto.html")
+
+@app.route("/data")
+def data():
+    return jsonify([{"coluna1":1,"coluna2":"Dados 1"},{"coluna1":3,"coluna2":"Dados 3"}])
